abb09ce91602c69d06d5b80ce7703a53b48ece7f
sadmaloy <rostislav_ivanov2015@mail.ru>
2024-08-23 11:50:27 +0300

    feat: meme quiz added

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+const prompt = inquirer.createPromptModule();
+
+const fs = require('fs');
+
+const quizMemes = JSON.parse(fs.readFileSync('./topics/memes.json', 'utf-8'));
+
+// console.log(Object.values(quizMemes[0]));
+
+prompt([...quizMemes
+]).then((answers) => console.log(answers));
